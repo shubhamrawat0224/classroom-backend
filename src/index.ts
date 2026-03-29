4a4a394cc0fe3258d98b541ffe3a186d4057768b
Shubham Rawat <shubhamrawat@Shubhams-MacBook-Air.local>
2026-03-29 19:07:24 +0530

    PR changes as per the comments

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+if (!process.env.FRONTEND_URL) {
+  throw new Error("FRONTEND_URL is not defined");
+}
+
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
